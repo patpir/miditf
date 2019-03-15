293996b378352f5c11c30f3dad441ac01b2db91d
Patrick Pirringer <patrick.pirringer@gmail.com>
2019-03-15 20:34:55 +0100

    Add major scale source

---
+++ sources/MajorScale.go
@@ -0,0 +1,60 @@
+package sources
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/patpir/miditf/core"
+	"github.com/patpir/miditf/blocks"
+)
+
+type majorScale struct {
+	baseTone  uint8
+}
+
+
+func newMajorScale(config []string) (blocks.Source, error) {
+	if len(config) != 1 {
+		return nil, errors.New("Invalid number of arguments")
+	}
+
+	baseTone, err := strconv.Atoi(config[0])
+	if err != nil {
+		return nil, err
+	}
+
+	scale := majorScale{}
+	scale.baseTone = uint8(baseTone)
+	return &scale, nil
+}
+
+func (scale *majorScale) Piece() *core.Piece {
+	notes := []*core.Note{
+		core.NewNote(0, scale.baseTone     , 80,   0,  15),
+		core.NewNote(0, scale.baseTone +  2, 80,  16,  31),
+		core.NewNote(0, scale.baseTone +  4, 80,  32,  47),
+		core.NewNote(0, scale.baseTone +  5, 80,  48,  63),
+		core.NewNote(0, scale.baseTone +  7, 80,  64,  79),
+		core.NewNote(0, scale.baseTone +  9, 80,  80,  95),
+		core.NewNote(0, scale.baseTone + 11, 80,  96, 111),
+		core.NewNote(0, scale.baseTone + 12, 80, 112, 127),
+	}
+
+	track := core.NewTrack()
+	for _, note := range notes {
+		track.AddNote(note)
+	}
+
+	piece := core.NewPiece()
+	piece.AddTrack(track)
+	return piece
+}
+
+func init() {
+	arguments := []blocks.Argument{
+		blocks.NewArgument("base-tone", "Tone at which the major scale starts - this is the lowest tone of the scale"),
+	}
+	details := blocks.NewDetails("major-scale", "Creates a major scale", arguments)
+	blocks.RegisterSource(details, newMajorScale)
+}
+
---
+++ sources/majorscale_test.go
@@ -0,0 +1,47 @@
+package sources
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/patpir/miditf/blocks"
+)
+
+func TestCreateMajorScaleSourceFromFactory(t *testing.T) {
+	// MIDI key 60 stands fo C4
+	src, err := blocks.CreateSource("major-scale", []string{"60"})
+	assert.Nil(t, err)
+	assert.NotNil(t, src)
+}
+
+func TestCreateMajorScaleSourceWihoutParams(t *testing.T) {
+	src, err := blocks.CreateSource("major-scale", []string{})
+	assert.NotNil(t, err)
+	assert.Nil(t, src)
+}
+
+func TestMajorScaleNotes(t *testing.T) {
+	src, err := blocks.CreateSource("major-scale", []string{"60"})
+	assert.Nil(t, err)
+	assert.NotNil(t, src)
+
+	piece := src.Piece()
+	assert.NotNil(t, piece)
+
+	tracks := piece.Tracks()
+	assert.Equal(t, 1, len(tracks))
+	track := tracks[0]
+	assert.NotNil(t, track)
+
+	notes := track.Notes()
+	assert.Equal(t, 8, len(notes))
+
+	keys := []int{ 60, 62, 64, 65, 67, 69, 71, 72 }
+	for index, note := range notes {
+		assert.Equal(t, uint8(keys[index]), note.Key())
+		assert.Equal(t, uint32(16 * index), note.StartTime())
+		assert.Equal(t, uint32(15), note.EndTime() - note.StartTime())
+	}
+}
+
