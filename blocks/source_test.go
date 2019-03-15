03da03ff9840e9507eb36e987a357fd588648550
Patrick Pirringer <patrick.pirringer@gmail.com>
2019-03-15 17:40:16 +0100

    Improve tests style

--- blocks/source_test.go
+++ blocks/source_test.go
@@ -21,6 +21,7 @@ func TestListSources(t *testing.T) {
 	details := Details{ identifier: "test-source", description: "Test Source", arguments: []Argument{} }
 	RegisterSource(details, mockSourceFactory)
 	sources := Sources()
+
 	assert.Equal(t, 1, len(sources))
 	assert.Equal(t, "test-source", sources[0].Identifier())
 	assert.Equal(t, "Test Source", sources[0].Description())
--- blocks/transformation_test.go
+++ blocks/transformation_test.go
@@ -12,15 +12,16 @@ func mockTransformationFactory(config []string) (Transformation, error) {
 
 
 func TestInvalidTransformationId(t *testing.T) {
-	src, err := CreateTransformation("transformation does not exist", []string{})
+	tf, err := CreateTransformation("transformation does not exist", []string{})
 	assert.NotNil(t, err)
-	assert.Nil(t, src)
+	assert.Nil(t, tf)
 }
 
 func TestListTransformations(t *testing.T) {
 	details := Details{ identifier: "test-tf", description: "Test Transformation", arguments: []Argument{} }
 	RegisterTransformation(details, mockTransformationFactory)
 	transformations := Transformations()
+
 	assert.Equal(t, 1, len(transformations))
 	assert.Equal(t, "test-tf", transformations[0].identifier)
 	assert.Equal(t, "Test Transformation", transformations[0].description)
