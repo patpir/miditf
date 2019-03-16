51b55df9ee2cfa29d06693f7741eb16c97dc5bed
Patrick Pirringer <patrick.pirringer@gmail.com>
2019-03-16 21:14:37 +0100

    Improve test coverage in core package

--- core/piece_test.go
+++ core/piece_test.go
@@ -6,8 +6,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreatePiece(t *testing.T) {
+func TestPiece(t *testing.T) {
 	piece := NewPiece()
 	assert.NotNil(t, piece)
+
+	assert.Equal(t, 0, len(piece.Tracks()))
+	track := NewTrack()
+	piece.AddTrack(track)
+	assert.Equal(t, 1, len(piece.Tracks()))
 }
 
--- core/track_test.go
+++ core/track_test.go
@@ -9,5 +9,10 @@ import (
 func TestCreateTrack(t *testing.T) {
 	track := NewTrack()
 	assert.NotNil(t, track)
+
+	assert.Equal(t, 0, len(track.Notes()))
+	note := NewNote(0, 60, 80, 0, 15)
+	track.AddNote(note)
+	assert.Equal(t, 1, len(track.Notes()))
 }
 
